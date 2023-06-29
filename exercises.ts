2aea53d94278bb55ade98140d2f187396cd61bba
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-06-29 14:16:38 -0500

    Add Person Class

--- exercises.ts
+++ exercises.ts
@@ -1,8 +1,5 @@
 // Exercise: TypeScript Function
 
-// This exercise focuses on creating a basic TypeScript function.
-// Follow the steps below to complete the exercise:
-
 // 1. Define a function called `sumNumbers` that takes two parameters: `num1` and `num2`, both of type `number`.
 //    The function should return the sum of the two numbers.
 // 2. Implement the function body so that it correctly calculates the sum of the numbers.
@@ -22,3 +19,38 @@ const b: number = 5;
 const result: number = sumNumbers(a, b);
 
 console.log(result);
+
+// Exercise: Create a TypeScript Class
+
+// 1. Define a class called `Person` with the following properties:
+//    - `name` of type `string`
+//    - `age` of type `number`
+//    - `email` of type `string`
+// 2. Implement a constructor for the `Person` class that accepts values for the `name`, `age`, and `email`
+// properties and initializes them.
+// 3. Add a method to the `Person` class called `introduce` that prints a brief introduction of the person.
+//    The introduction should include their name, age, and email address.
+// 4. Create an instance of the `Person` class with your own information.
+// 5. Call the `introduce` method on the created instance to print your introduction.
+
+class Person {
+  name: string;
+  age: number;
+  email: string;
+
+  constructor(name: string, age: number, email: string) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+  }
+
+  // void: the function doesn't return any value;
+  introduce(): void {
+    console.log(
+      `Hi, my name is ${this.name}. I'm ${this.age} years old, and my email address is ${this.email}.`
+    );
+  }
+}
+
+const person = new Person("John Doe", 64, "johndoe@example.com");
+person.introduce();
