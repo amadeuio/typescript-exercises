374440bdda9bfe2039f5bd1af4e846fb59f14f5c
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-07-20 14:37:47 -0500

    Update findFirstNonRepeatedCharacter

--- solutions/findFirstNonRepeatedCharacter.ts
+++ solutions/findFirstNonRepeatedCharacter.ts
@@ -5,19 +5,19 @@
 function findFirstNonRepeatedCharacter(input: string): string | null {
   const charCount: { [key: string]: number } = {};
 
-  // Count the occurrences of each character in the input string and store in charCount object
+  // count the occurrences of each character in the input string and store in charCount object
   for (const char of input) {
     charCount[char] = (charCount[char] || 0) + 1;
   }
 
-  // Find the first character with a count of 1
+  // find the first character with a count of 1
   for (const char of input) {
     if (charCount[char] === 1) {
       return char;
     }
   }
 
-  // If no non-repeated character found, return null
+  // if no non-repeated character found, return null
   return null;
 }
 
