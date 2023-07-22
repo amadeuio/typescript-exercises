67032ecec3e44b393d1fb2e8c15dff6ee60184be
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-07-22 17:52:03 -0500

    Add stackDataStructure solution

--- solutions/stackDataStructure.ts
+++ solutions/stackDataStructure.ts
@@ -1,3 +1,36 @@
 // Stack Data Structure
 
 // Solution:
+
+class Stack<T> {
+  // T[] is an array of generic type
+  items: T[];
+
+  // the constructor creates objects with a key 'items' and value '[]', which will be the array
+  // where we will create our stack
+  constructor() {
+    this.items = [];
+  }
+
+  push(item: T): void {
+    this.items.push(item);
+  }
+
+  pop(): T | undefined {
+    return this.items.pop();
+  }
+
+  isEmpty(): boolean {
+    return this.items.length === 0;
+  }
+
+  peek(): T | undefined {
+    return this.items.slice(-1)[0];
+  }
+
+  size(): number {
+    return this.items.length;
+  }
+}
+
+export {};
